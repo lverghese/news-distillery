5e0accb355dd0c8afd696330343f97f25c98116c
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-11-22 14:15:16 -0500

    fixing card width

--- client/src/components/ArtCard.js
+++ client/src/components/ArtCard.js
@@ -5,7 +5,7 @@ import { Card } from 'react-bootstrap';
 const ArtCard = (props) => {
     console.log(props)
     return (
-        <Card className="Card" border="secondary" style={{width: '26rem'}}>
+        <Card className="Card" border="secondary" style={{width: '28rem'}}>
             <Card.Header>{props.title}</Card.Header>
             <Card.Text>{props.description}</Card.Text>
             <Card.Link href={props.url}><Card.Img src={props.urlToImage}></Card.Img></Card.Link>
