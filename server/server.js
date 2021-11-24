9a7d5bbf1611d396694e2dc5d47cb82e880afa55
caroline <cluce059@gmail.com>
2021-11-24 10:41:34 -0500

    heroku

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const {typeDefs, resolvers} = require('./schemas');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
 
+const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -19,7 +19,7 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.set("port", PORT);
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
